e202f0ae6ae3e6a0f8f2cdc47c434ea7cadad62f
lbito <lbit@protonmail.com>
2024-12-04 14:13:20 +1000

    refactored day 3 and added pt2

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	utils "aoc-24-lbit/internal"
 	"fmt"
-	"os"
-	"path/filepath"
 	"regexp"
 	"strconv"
 )
@@ -16,36 +15,55 @@ type Operation struct {
 
 func main() {
 	fmt.Println("Day 3")
-	rawString, _ := LoadRaw("3.txt")
-	opRegEx := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-	argRegEx := regexp.MustCompile(`\((\d+),(\d+)\)`)
+	rawString, _ := utils.LoadRaw("3.txt")
+	opRegEx := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	opNameRegEx := regexp.MustCompile(`^([\w']+)\(`)
+	mulRegEx := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+
 	matches := opRegEx.FindAllString(rawString, -1)
 	ops := make([]Operation, len(matches))
 
 	for i, match := range matches {
-		argMatches := argRegEx.FindStringSubmatch(match)
-		a, _ := strconv.Atoi(argMatches[1])
-		b, _ := strconv.Atoi(argMatches[2])
-		ops[i] = Operation{op: "mul", a: a, b: b}
+		opname := opNameRegEx.FindStringSubmatch(match)
+		if opname[1] == "mul" {
+			mulArgs := mulRegEx.FindStringSubmatch(match)
+			a, _ := strconv.Atoi(mulArgs[1])
+			b, _ := strconv.Atoi(mulArgs[2])
+			ops[i] = Operation{op: opname[1], a: a, b: b}
+		} else {
+			ops[i] = Operation{op: opname[1]}
+		}
 	}
-	// fmt.Print(ops)
 	partOneSolution := partOne(ops)
 	fmt.Println("Part One Solution:", partOneSolution)
-}
-
-func LoadRaw(fname string) (string, error) {
-	fPath := filepath.Join("data", fname)
-	content, err := os.ReadFile(fPath)
-	if err != nil {
-		return "", fmt.Errorf("error reading file: %w", err)
-	}
-	return string(content), nil
+	partTwoSolution := partTwo(ops)
+	fmt.Println("Part Two Solution:", partTwoSolution)
 }
 
 func partOne(ops []Operation) int {
 	sum := 0
 	for _, op := range ops {
-		sum += (op.a * op.b)
+		if op.op == "mul" {
+			sum += (op.a * op.b)
+		}
+	}
+	return sum
+}
+
+func partTwo(ops []Operation) int {
+	mul := true
+	sum := 0
+	for _, op := range ops {
+		switch op.op {
+		case "do":
+			mul = true
+		case "don't":
+			mul = false
+		case "mul":
+			if mul {
+				sum += (op.a * op.b)
+			}
+		}
 	}
 	return sum
 }
